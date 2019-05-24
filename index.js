fc2fc6550419abf29fa220b624bce51c664cdcf6
陈帅 <qixian.cs@outlook.com>
2019-05-24 16:46:26 +0800

    merge-umi-mock-data

--- index.js
+++ index.js
@@ -2,37 +2,42 @@ const rollup = require("rollup");
 const json = require("rollup-plugin-json");
 const chalk = require("chalk");
 const babel = require("rollup-plugin-babel");
-const builtins = require("rollup-plugin-node-builtins");
 const glob = require("glob");
 const path = require("path");
 const fs = require("fs");
-const slash = require('slash2');
+const slash = require("slash2");
 
-const allMock = path.join(__dirname, "./allMock.js");
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
+
+const firstUpperCase = pathString => {
+  return pathString
+    .replace(".", "")
+    .split(/\/|\-/)
+    .map(s => s.toLowerCase().replace(/( |^)[a-z]/g, L => L.toUpperCase()))
+    .filter(s => s)
+    .join("");
+};
+const getArrayLastTwo = fileNameArray => {
+  const nameArray = [];
+  nameArray.push(fileNameArray.pop());
+  nameArray.push(fileNameArray.pop());
+  return firstUpperCase(nameArray.join("/"));
+};
+
+const allMock = path.join(__dirname, "./allMock.ts");
 
 const inputOptions = {
   input: allMock,
   onwarn(warning) {},
   plugins: [
-    builtins(),
     json({
       preferConst: true, // Default: false
       indent: "  "
     }),
     babel({
+      extensions,
       babelrc: false,
-      runtimeHelpers: true,
-      presets: [
-        [
-          "@babel/env",
-          {
-            targets: {
-              node: "6.11.5"
-            },
-            modules: false
-          }
-        ]
-      ]
+      presets: ["@babel/env", "@babel/preset-typescript"]
     })
   ]
 };
@@ -45,25 +50,38 @@ const importMockFiles = (mockFiles, mockPath) => {
   const importString = [];
   const dataString = [];
   mockFiles.forEach(filePath => {
-    const fileName = filePath.replace(".js", "");
+    const fileName = getArrayLastTwo(
+      filePath
+        .replace(".ts", "")
+        .replace("_", "")
+        .split("/")
+    );
     importString.push(
-      `import ${fileName} from "${slash(path.join(mockPath, filePath))}";`
+      `import ${fileName} from "${slash(
+        path.join(mockPath, filePath.replace(".ts", ""))
+      )}";`
     );
     dataString.push(`...${fileName}`);
   });
   return `
-${importString.join("\n")}
+  ${importString.join("\n")}
 
-const data = {${dataString.join(",")}};
+  const data = {${dataString.join(",")}};
 
-export default data;
-`;
+  export default data;
+  `;
 };
 
 async function build(mockPath, outputfile) {
-  const mockFiles = glob.sync("**/*.js", {
+  let mockFiles = glob.sync("**/*.ts", {
     cwd: mockPath
   });
+  mockFiles = mockFiles.concat(
+    glob.sync("../src/**/**/_mock.ts", {
+      cwd: mockPath,
+      ignore: "* node_modules"
+    })
+  );
   console.log("get files: " + chalk.green(mockFiles.join(", ")));
   const allMockText = importMockFiles(mockFiles, mockPath);
   fs.writeFileSync(allMock, allMockText);
@@ -77,5 +95,9 @@ async function build(mockPath, outputfile) {
   console.log(chalk.yellow("-".repeat(80)));
   console.log(chalk.blue("finish merge file"));
 }
+build(
+  path.join("/Users/qixian.cs/Documents/GitHub/ant-design-pro", "./mock"),
+  "./index.mock.js"
+);
 
 module.exports = build;
